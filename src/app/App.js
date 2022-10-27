9a5486a238ee295a72121a5727a9074c17c8e8b9
Patricia Oliveira <patriciia.mota@gmail.com>
2022-10-27 18:21:33 +0100

    Move App component for a app folder

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Are you ready to be challenged?</header>
+    </div>
+  );
+}
+
+export default App;
