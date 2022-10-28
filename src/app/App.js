c2c0efd5d9d2eb15306cc1194e0ac079991afce0
Patricia Oliveira <patriciia.mota@gmail.com>
2022-10-28 16:07:26 +0100

    Add initial implementation of landing page

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import "animate.css";
+import { WodMode} from "../wodMode/WodMode";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header animate__animated animate__bounce">Are you ready to be challenged?</header>
+        <WodMode/>
     </div>
   );
 }
--- src/wodMode/WodMode.js
+++ src/wodMode/WodMode.js
@@ -1,6 +1,31 @@
 import React from "react";
-import "WodMode.css";
+import "./WodMode.css";
 
-function WodMode() {
-    return (<div></div>)
+export function WodMode() {
+    return (
+        <div className="wod-mode container">
+            <div className="row row-cols-2 gy-4 w-50">
+                <div className="col">
+                    <div className="card p-5 mode-card">
+                        <div className="card-body">AMRAP</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card p-5 mode-card">
+                        <div className="card-body">RFT</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card p-5 mode-card">
+                        <div className="card-body">EMOM</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card p-5 mode-card">
+                        <div className="card-body">TABATA</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
