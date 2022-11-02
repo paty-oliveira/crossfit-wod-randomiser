3f52c42a8666542531c721722b483bfb08b2356b
Patricia Oliveira <patriciia.mota@gmail.com>
2022-11-02 18:36:00 +0000

    Add routes

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import "animate.css";
 import {Home} from "../home/Home";
+import {Wod} from "../wod/Wod";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-      <Home />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/wod" element={<Wod />} />
+    </Routes>
   );
 }
 
